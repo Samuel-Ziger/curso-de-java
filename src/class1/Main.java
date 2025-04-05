0311a1e63a4568f34bf85e2cb05f40deccfaf89c
samuel-ziger <srziger3@gmail.com>
2025-04-05 13:30:33 -0300

    Primeiro commit

---
+++ src/class1/Main.java
@@ -0,0 +1,10 @@
+package class1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo");
+	}
+int idade = 25; // Declaração de variável de idade: int = variável de número inteiro.
+
+}
