28746efd65e1ad6ea2d62fc9ef23b0aa7b852767
samuel-ziger <srziger3@gmail.com>
2025-04-05 14:43:14 -0300

    Código e explicação de variáveis

--- src/class1/Main.java
+++ src/class1/Main.java
@@ -5,6 +5,20 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo");
 	}
-int idade = 25; // Declaração de variável de idade: int = variável de número inteiro.
+int idade = 25; // Declaração de variável de idade: int = variável de número inteiro.   
+//idade e o  nome da variável e após ela o = e o valor e sempre terminar em '';''
+
+double altura = 1.60; // Declaração de variável altura: double = variável de número com casa decimal. 
+// altura e o nome da variável e ós ela o = e o valor e sempre terminar em '';''
+
+char sexo = 'F'; // Declaração de variável do tipo caractere. 
+// 'char' é um tipo de dado que armazena um único caractere, 
+// podendo ser símbolo, letra ou número. 
+// 'sexo' é o nome da variável, e 'F' é o valor atribuído a ela.E sempre terminar em '';''
+
+
+// então o esquema de uma variável e <tipo> <nome> = <Valor inicial >; 
+//A variável fica Hospedada da mémoriam Ram a onde e executada e conforme isso a variável também tem um endereço quye será visto mais a frente 
 
 }
+ 
