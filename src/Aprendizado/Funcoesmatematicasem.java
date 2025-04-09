17b5b13734c53d2e3edbe228f63fd5efeaa8afb5
samuel-ziger <srziger3@gmail.com>
2025-04-09 17:41:56 -0300

    Funções matemáticas em Java Começo

---
+++ src/Aprendizado/Funcoesmatematicasem.java
@@ -0,0 +1,9 @@
+package Aprendizado;
+
+public class Funcoesmatematicasem {
+    
+    public static void main(String[] args) {
+        //Vamos começar com as Funções matemáticas dentro de Java.
+        
+    }
+}
