3203d532a409ed9f42243eceb5619a353d801749
samuel-ziger <srziger3@gmail.com>
2025-06-19 21:45:00 -0300

    nova função nas strings

--- src/Aprendizado/FuncoeosParaString.java
+++ src/Aprendizado/FuncoeosParaString.java
@@ -17,12 +17,14 @@ public class FuncoeosParaString {
         String s02 = original.toUpperCase(); // fazendo a conversão de todos os caracteres para maiusculo.
         String s03 = original.trim(); // fazendo as String continuarem da mesma forma mas agora sem espaçoes inutéis.
         String s04 = original.substring(2); // Fazendo com que pegue do segundo caracter em diante e crie uma nova string. senod que no exemplo as letrsd abc vai ser assim : --> 0 = a, b = 1 e c =2 então o segunda caracter e o "c".
+        String s05 = original.substring(2, 9); //Fazendo a string corta entre os caracte
+
         System.out.println(s01);
         System.out.println("Original: - " + original + "-"); // aqui foi feita a concatenação usando o "+" pra juntar a String com o que vai está esciroto o "-" serve somente pra mostrar a onde a sring começa e onde termina.
         System.out.println("toLowerCase: - " + s01 + "-"  ); //  aqui foi feita a concatenação usando o "+" pra juntar a String com o que vai está escirotoo "-" serve somente pra mostrar a onde a sring começa e onde termina.
         System.out.println("toUpperCase: - " + s02 + "-"  ); //  aqui foi feita a concatenação usando o "+" pra juntar a String com o que vai está escirotoo "-" serve somente pra mostrar a onde a sring começa e onde termina.
         System.out.println("trim: - " + s03 + "-"  );// va i remover os espaços após a String.
         System.out.println("Substring(2): - " + s04 + "-"  ); // A após o segundo caracter sendo ele a letra "C" ele ira mostra o restante da String.
-       
+        System.out.println("Substring(2): - " + s05 + "-"  ); // Aqui vai acontecer que ele sera feito o recorte
     }
 }
